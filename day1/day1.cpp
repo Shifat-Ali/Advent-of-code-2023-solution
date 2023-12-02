ab7d337a6eaad2da404b6207d8d01c02d52ba82e
Shifat <a.shifat@iitg.ac.in>
2023-12-02 19:44:38 +0530

    My approach to day 1

---
+++ day1/day1.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    FILE *file = fopen("input.txt", "r");
+    FILE *out = fopen("output.txt", "w");
+    long long sum = 0;
+    char c;
+    int first, last;
+    first = last = -1;
+    int count = 0;
+    while ((c = fgetc(file)) != EOF)
+    {
+        if (c - '0' <= 9 && c - '0' >= 0)
+        {
+            if (first == -1)
+                first = c - '0';
+            else
+                last = c - '0';
+        }
+        else if (c == '\n')
+        {
+            count++;
+            last = last == -1 ? first : last;
+            fprintf(out, "%d. first: %d, last: %d -> %d\n", count, first, last, (first * 10 + last));
+            sum += (long long)(first * 10 + last);
+            first = last = -1;
+        }
+    }
+    count++;
+    last = last == -1 ? first : last;
+    fprintf(out, "%d. first: %d, last: %d -> %d\n", count, first, last, (first * 10 + last));
+    sum += (long long)(first * 10 + last);
+    first = last = -1;
+    fclose(file);
+    fprintf(out, "Total SUm: %ld\n", sum);
+}
